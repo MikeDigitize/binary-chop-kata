b4eb2de2c36fb823730a7956f1e40a0421c45981
MikeDigitize <chad_01@hotmail.co.uk>
2020-11-30 18:36:21 +0000

    returns the index without indexof

--- __tests__/binary-chop.test.js
+++ __tests__/binary-chop.test.js
@@ -7,4 +7,34 @@ describe('A binary chop kata', function() {
         const result = binary(target, array)
         expect(result).toBe(-1);
     });
+    it('should return -1 when the array does not contain the target', function () {
+        const target = 1;
+        const array = [2];
+        const result = binary(target, array)
+        expect(result).toBe(-1);
+    });
+    it('should return -1 when the array does not contain the target', function () {
+        const target = 1;
+        const array = [2,3,4];
+        const result = binary(target, array)
+        expect(result).toBe(-1);
+    });
+    it('should return the index when the array does contain the target', function () {
+        const target = 1;
+        const array = [1];
+        const result = binary(target, array)
+        expect(result).toBe(0);
+    });
+    it('should return the index when the array does contain the target', function () {
+        const target = 1;
+        const array = [2,3,4,1];
+        const result = binary(target, array)
+        expect(result).toBe(3);
+    });
+    it('should return the index when the array does contain the target', function () {
+        const target = 2;
+        const array = [1,2,3,4];
+        const result = binary(target, array)
+        expect(result).toBe(1);
+    });
 });
--- binary.js
+++ binary.js
@@ -6,10 +6,18 @@
  * 
  * The signature will logically be:
  * chop(int, array_of_int)  -> int
+ * 
+ * assume an array does not have helper methods
  */
 
 function binary(target, array) {
-    return array.indexOf(target);
+    let result = -1;
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === target) {
+            result = i;
+        }
+    }
+    return result;
 }
 
 module.exports = binary;
